281fb2c9ab342699972ff9a29160ff258000278c
Iddo Drori <idrori@mit.edu>
2023-06-18 09:52:41 -0400

    Update main.py

--- code/main.py
+++ code/main.py
@@ -16,7 +16,7 @@ import os
 logging.basicConfig(filename='log.txt', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 os.environ['OpenAI_API_Key'] = 'sk-'
 os.environ['Prompt_Engine'] = 'gpt-4'
-os.environ['Embedding_Engine'] = 'text-embedding-ada-002'
+os.environ['Embedding_Engine'] = 'text-embedding-ada-002' # for few-shot
 os.environ['Grading_Engine'] = 'gpt-4' # auto-grading compares with ground truth solution
 os.environ['Experts_Engine'] = 'gpt-4'
 openai.api_key = os.getenv('OpenAI_API_Key')
