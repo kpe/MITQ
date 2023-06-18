5f6709045be4830ba325a75ca5eb36cb1a2ecc8c
Iddo Drori <idrori@mit.edu>
2023-06-18 18:47:06 -0400

    Update experts.py
    Using function calling for robustness

--- code/experts.py
+++ code/experts.py
@@ -7,22 +7,38 @@ import csv
 import math
 from grade import num_tokens_from_messages
 import os
+from typing import List
+from pydantic import BaseModel
+import json
 
 openai.api_key = os.getenv('OpenAI_API_Key')
+
+class Experts(BaseModel):
+    expert_1: str
+    expert_2: str
+    expert_3: str
 
 @backoff.on_exception(backoff.expo, openai.error.RateLimitError)
 def get_experts(department, course_name, question, num_experts=3, max_tokens = 8192):
     generic_expert = f"an MIT Professor of {department} teaching the {course_name} course"
-    messages = [{"role":"system", "content": "You are " + generic_expert + "\n"},
-                {"role":"user", "content": f"Give an educated guess of who are {num_experts} experts most capable of solving the following question.\n Question: {question}.\n Return a comma-separated list of {num_experts} names."}
-    ]
-    try:
-        completion = openai.ChatCompletion.create(
-                model=os.getenv('Experts_Engine'),
+    try:              
+        response = openai.ChatCompletion.create(
+                model="gpt-4-0613",
                 temperature=0,
-                max_tokens=max_tokens - num_tokens_from_messages(messages),
-                messages=messages)
-        experts = generic_expert + ", " + completion["choices"][0]["message"]["content"]        
+                messages=[
+                {"role":"user", "content": f"You are " + generic_expert + ". Give an educated guess of who are {num_experts} experts most capable of solving the following question.\n Question: {question}.\n Return a comma-separated list of {num_experts} names."}],
+                functions=[
+                {
+                "name": "get_named_experts_for_question",
+                "description": "Get named experts for answering a question",
+                "parameters": Experts.schema()
+                }
+                ],
+                function_call={"name": "get_named_experts_for_question"}
+                )
+        arguments_str = response["choices"][0]["message"]["function_call"]["arguments"]
+        arguments = json.loads(arguments_str)
+        experts = (generic_expert + ', ' + arguments["expert_1"] + ', ' + arguments["expert_2"] + ', ' + arguments["expert_3"])
         return experts
     except openai.error.APIError as e:
         time.sleep(45)
