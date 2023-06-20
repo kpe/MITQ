4d11955edd26100bcbe5168b07b9e60ed562e917
Iddo Drori <idrori@mit.edu>
2023-06-20 00:30:03 -0400

    Update few_shot.py

--- code/few_shot.py
+++ code/few_shot.py
@@ -9,7 +9,7 @@ from grade import num_tokens_from_messages
 import os
 
 openai.api_key = os.getenv('OpenAI_API_Key')
-def few_shot_response(system, question, fs_qs, cot=False, max_tokens=8192):
+def few_shot_response(system, question, fs_qs, cot=True, max_tokens=8192):
     #course_num = adjusted_course_num(course)
     #similar_qs = get_n_similar_questions(course, number, n)
     messages = []
